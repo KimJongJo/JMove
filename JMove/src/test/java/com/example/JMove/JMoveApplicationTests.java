9dffd33768ee2c2c838bf57d3312c42bf3b6e0a6
KimJongJo <desk1614@gmail.com>
2025-01-15 00:00:11 +0900

    first commit

---
+++ JMove/src/test/java/com/example/JMove/JMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JMove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JMoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
